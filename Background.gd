1ebce7d31b81548e9822b82f982e31e014680745
Jake Pearse <jake.pearse@scitegrity.com>
2021-12-17 19:42:35 +0000

    add floor and background

---
+++ Background.gd
@@ -0,0 +1,19 @@
+extends ParallaxBackground
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export var spd: int
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _process(delta):
+	scroll_offset.x -= spd * delta
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
