dc8519baa4a52defd5c9299142d45c100d9119f9
Jake Pearse <jake.pearse@scitegrity.com>
2021-12-18 21:51:17 +0000

    basic transition

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -1,11 +1,16 @@
 extends Control
 
 func _ready():
-	pass # Replace with function body.
+	$CanvasLayer2/Transition.In(1.5)
 
 func _on_Button_pressed():
-	get_tree().change_scene("res://Scenes/Level.tscn")
-
+	$CanvasLayer2/Transition.Out(2.0)
+	
 
 func _on_MenuMusic_finished():
 	$MenuMusic.play()
+
+
+func _on_Transition_scene_changed():
+	print('c')
+	get_tree().change_scene("res://Scenes/Level.tscn")
