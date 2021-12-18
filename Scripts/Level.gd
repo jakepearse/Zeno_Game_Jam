59148df27ba9eda1d23e878d919e0a3b9abf72f5
Jake Pearse <jake.pearse@scitegrity.com>
2021-12-18 12:09:29 +0000

    keyssprites to instance

--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -3,25 +3,28 @@ extends Node2D
 const charHeight = 300 #Starting y-coord of Characters
 const spriteHeight = 560 #y-coord of key sprites
 const character_width = 64
-#var difficulty = 0 #An int that increments with every timeout of PlayerTimer.
-#Evaluated in a match (think switch) statement to determine the next modifier.
 
 onready var Character = preload("res://Scenes/Character.tscn")
 onready var Obstacle = preload("res://Scenes/Obstacle.tscn")
 
-onready var keySpriteA = preload("res://Scenes/Key Sprites/Key Sprite A.tscn")
-onready var keySpriteD = preload("res://Scenes/Key Sprites/Key Sprite D.tscn")
-onready var keySpriteS = preload("res://Scenes/Key Sprites/Key Sprite S.tscn")
-onready var keySpriteF = preload("res://Scenes/Key Sprites/Key Sprite F.tscn")
-onready var keySpriteH = preload("res://Scenes/Key Sprites/Key Sprite H.tscn")
-onready var keySpriteJ = preload("res://Scenes/Key Sprites/Key Sprite J.tscn")
-onready var keySpriteK = preload("res://Scenes/Key Sprites/Key Sprite K.tscn")
-onready var keySpriteL = preload("res://Scenes/Key Sprites/Key Sprite L.tscn")
+onready var KeySprite = preload("res://Scenes/KeySprite.tscn")
+
+## load the textures so we can switch the keysprites in per instance
+onready var key_sprite_textures = {
+	KEY_A: { 'up': load("res://Sprites/Test/A-Up.png"), 'down': load("res://Sprites/Test/A-Down.png") },
+	KEY_S: { 'up' : load("res://Sprites/Test/S-Up.png"), 'down': load("res://Sprites/Test/S-Down.png") },
+	KEY_D: { 'up' : load("res://Sprites/Test/D-Up.png"), 'down': load("res://Sprites/Test/D-Down.png") },
+	KEY_F: { 'up' : load("res://Sprites/Test/F-Up.png"), 'down': load("res://Sprites/Test/F-Down.png") },
+	KEY_H: { 'up' : load("res://Sprites/Test/H-Up.png"), 'down': load("res://Sprites/Test/H-Down.png") },
+	KEY_J: { 'up' : load("res://Sprites/Test/J-Up.png"), 'down': load("res://Sprites/Test/J-Down.png") },
+	KEY_K: { 'up' : load("res://Sprites/Test/K-Up.png"), 'down': load("res://Sprites/Test/K-Down.png") },
+	KEY_L: { 'up' : load("res://Sprites/Test/L-Up.png"), 'down': load("res://Sprites/Test/L-Down.png") }
+}
 
 var keyMap = [KEY_A, KEY_S, KEY_D, KEY_F, KEY_H, KEY_J, KEY_K, KEY_L]
 
 var c:Node #New character spawns will be assigned to this variable.
-var keySprite:Node #New Keysprite spawns will be assigned to this var.
+var keySprite:Sprite#:Sprite #New Keysprite spawns will be assigned to this var.
 	
 func _ready():
 	randomize() #Set a random seed for RNG
@@ -48,21 +51,10 @@ func spawn_character(pos):
 	$CharecterContainer.add_child(c) ## if we keep them all together in a node its easy to count them
 
 func assign_sprite(key):
-	match key: #Select a node, assign it to keySprite
-		KEY_A:
-			keySprite = keySpriteA.instance()
-		KEY_S:
-			keySprite = keySpriteS.instance()
-		KEY_D:
-			keySprite = keySpriteD.instance()
-		KEY_F:
-			keySprite = keySpriteF.instance()
-		KEY_H:
-			keySprite = keySpriteH.instance()
-		KEY_J:
-			keySprite = keySpriteJ.instance()
-		KEY_K:
-			keySprite = keySpriteK.instance()
-		KEY_L:
-			keySprite = keySpriteL.instance()
-	add_child(keySprite)
+	var k = KeySprite.instance()
+	keySprite = k.get_node("Sprite")
+	keySprite.key = key
+	keySprite.up_sprite = key_sprite_textures[key].up
+	keySprite.down_sprite = key_sprite_textures[key].down
+	## the keySprite is getting positioned in the parent function, on_player_timeout
+	add_child(k)
